b26bb0842d87a402ef09d2842d4dae28f24fbf5a
weilai0980 <tian.guo0980@gmail.com>
2017-04-14 16:26:45 +0200

    wide and embedding network

---
+++ utils.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+import matplotlib as mplt
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+import matplotlib
+from matplotlib.ticker import FuncFormatter
+import matplotlib.pyplot as plt
+# matplotlib.style.use('ggplot')
+import itertools
+
+def y_distribution_plot( dta, valset ):
+    
+    y_pos = np.arange(len( valset ))
+    cnt=[0 for _ in valset]
+    tot_cnt= len(dta)
+    
+    for i in dta:
+        cnt[int(i)]= cnt[int(i)]+1 
+
+    for i in range(len(valset)):
+        cnt[i]= cnt[i]*1.0/tot_cnt
+
+    label_set= ['18-24','25-34','35-44','45-54','55-64','65-74','75-84','85+']
+    fig = plt.figure()
+    plt.bar(y_pos, cnt, align='center', alpha=0.5)
+    plt.xticks(y_pos, label_set, rotation=70)
+    plt.ylim([0,0.3])
+    plt.ylabel('% of data instances w.r.t. \n each age label')
+    plt.xlabel('Age labels')
+# plt.title('Age label')
+#     fig.savefig('./results/classDis.jpg', format='jpg', bbox_inches='tight')
+    
